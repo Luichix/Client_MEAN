5d3238c6ce8159e4186833f277d38a8df6309e30
Luichix <luigui.rex@gmail.com>
2021-01-25 09:36:21 -0600

    angular client

---
+++ src/app/components/employee/employee.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service'
+import { NgForm } from '@angular/forms'
+import { Employee } from 'src/app/models/employee';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+  constructor(public employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+       this.getEmployees()
+  }
+
+  getEmployees(){
+    this.employeeService.getEmployees().subscribe(
+      res => {
+        this.employeeService.employees = res
+      },
+      err => console.log(err)
+    )
+  }
+
+  addEmployee(form: NgForm ){
+    if(form.value._id){
+      this.employeeService.putEmployee(form.value).subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      )
+    } else {
+      this.employeeService.createEmployee(form.value).subscribe(
+        res => {
+          this.getEmployees()
+          form.reset()
+        },
+        err => console.log(err)
+      )
+    }
+  }
+
+  deleteEmployee(id:string) {
+    if (confirm('Are you sure you want to delete it?')){
+      this.employeeService.deleteEmployee(id)
+      .subscribe(
+        res => {
+          this.getEmployees() 
+        },
+        err => console.log(err)
+      )
+    }  
+  }
+
+  editEmployee(employee: Employee){
+    this.employeeService.selectedEmployee= employee
+  }
+
+  resetForm (form: NgForm){
+    form.reset() 
+  }
+}
+ 
